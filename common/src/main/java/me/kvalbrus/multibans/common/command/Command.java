eeea364cfe77039f0031c17e35325950987643a6
kvalbrus <kvalbrus@gmail.com>
2023-04-04 23:21:37 +0500

    Add command system

---
+++ common/src/main/java/me/kvalbrus/multibans/common/command/Command.java
@@ -0,0 +1,43 @@
+package me.kvalbrus.multibans.common.command;
+
+import java.util.List;
+import java.util.HashMap;
+import lombok.Getter;
+import me.kvalbrus.multibans.api.CommandSender;
+import me.kvalbrus.multibans.common.managers.PluginManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class Command {
+
+    @Getter
+    private final PluginManager pluginManager;
+
+    @Getter
+    private final String name;
+
+    @Getter
+    private final String permission;
+
+    private final HashMap<String, Command> subcommands;
+
+    public Command(@NotNull PluginManager pluginManager,
+                   @NotNull String name,
+                   @NotNull String permission,
+                   @Nullable HashMap<String, Command> subcommands) {
+        this.pluginManager = pluginManager;
+        this.name = name;
+        this.permission = permission;
+        this.subcommands = subcommands;
+    }
+
+    public abstract boolean execute(@NotNull CommandSender sender,
+                                    String[] args);
+
+    public abstract List<String> tab(@NotNull CommandSender sender,
+                                     String[] args);
+
+    public Command getSubCommand(@NotNull String arg) {
+        return this.subcommands != null ? this.subcommands.get(arg) : null;
+    }
+}
