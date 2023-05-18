566ae7a0e129647e8bae103950cd2af65e46b568
kvalbrus <kvalbrus@gmail.com>
2023-05-18 21:35:56 +0500

    Change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,41 @@ plugins {
 
 val _version = "0.1.0"
 val _group = "me.kvalbrus"
+val modules = listOf("api", "common")
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    this.version = _version
+
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+
+    publications {
+        modules.forEach { moduleName ->
+            val module = project(":$moduleName")
+
+            create<MavenPublication>(moduleName) {
+                groupId = "me.kvalbrus.multibans" // Замените на свой groupId
+                artifactId = moduleName
+                version = "0.1.0" // Замените на нужную версию
+
+                from(module.components["java"])
+            }
+//            register(moduleName, MavenPublication::class) {
 //
-//subprojects {
-//    apply(plugin = "java")
-//    apply(plugin = "maven-publish")
-//
-//    this.version = _version
-//
+//            }
+        }
+    }
+}
+
 //    configure<PublishingExtension> {
 //        publications {
 //            create<MavenPublication>("maven") {
@@ -28,34 +56,34 @@ val _group = "me.kvalbrus"
 //            }
 //        }
 //    }
+
+
+//configure<PublishingExtension> {
+//    apply(plugin = "maven-publish")
+//    repositories {
+//        maven {
+//            url = uri("https://jitpack.io")
+//        }
+//    }
+//
+//    publications {
+//        create<MavenPublication>("api") {
+//            from(components["java"])
+//
+//            groupId = "com.github.kvalbrus"
+//            artifactId = "MultiBans"
+//            version = _version
+//        }
+//
+////        create<MavenPublication>("common") {
+////            from(components["java"])
+////
+////            groupId = "com.github.kvalbrus.multibans"
+////            artifactId = "common"
+////            version = _version
+////        }
+//    }
 //}
-
-configure<PublishingExtension> {
-    apply(plugin = "maven-publish")
-    repositories {
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-
-    publications {
-        create<MavenPublication>("api") {
-            from(components["java"])
-
-            groupId = "me.kvalbrus.multibans"
-            artifactId = "api"
-            version = _version
-        }
-
-        create<MavenPublication>("common") {
-            from(components["java"])
-
-            groupId = "me.kvalbrus.multibans"
-            artifactId = "common"
-            version = _version
-        }
-    }
-}
 
 tasks.withType(ShadowJar::class.java) {
     archiveFileName.set("MultiBans-${_version}.jar")
